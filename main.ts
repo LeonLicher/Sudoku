5cc1f344dd3ae1a2e55a07e29bf7ff2b3c6ad330
Leon Licher <licher.leon@gmail.com>
2023-12-16 16:12:59 +0100

    klappt immernoch nicht

--- main.ts
+++ main.ts
@@ -1,27 +1,30 @@
-import express from "express";
-import path, { dirname } from "path";
-import * as url from 'url';
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path, { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const port = 8080;
 
-// Get the directory path of the current module
-const currentModulePath = url.fileURLToPath(import.meta.url);
-const __dirname = dirname(currentModulePath);
+// Serve static files from the "public" directory
+app.use(express.static(join(__dirname, '..', 'public')));
 
-// Resolve the absolute path for the public directory
-const publicPath = path.resolve(__dirname, 'public');
-
-// Serve static files from the public directory
-app.use(express.static(path.join(__dirname, '..', 'public')));
+// Serve static files from the "dist/public" directory
+app.use(express.static(join(__dirname, 'public')));
 
 // Define a route for the root URL
-app.get("/", (req, res) => {
-    // Send the index.html file from the parent directory
-    res.status(200).sendFile(path.join(__dirname, '..', 'public', "index.html"));
+app.get('/', (req, res) => {
+  // Send the index.html file from the "public" directory
+  res.status(200).sendFile(join(__dirname, '..', 'public', 'index.html'));
 });
+app.get("/public/styles.css", (req, res) => {
+    res.type("text/css");
+    res.sendFile(path.join(__dirname, 'public', 'styles.css'));
+  });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
