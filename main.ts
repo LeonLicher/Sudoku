b35b189e4055e8edaf94ee4c12bb13b22a9d8f5c
LeonLicher <licher.leon@gmail.com>
2023-12-18 17:57:57 +0100

    saving for restart of my ide

--- main.ts
+++ main.ts
@@ -1,30 +1,32 @@
-import express from 'express';
-import { fileURLToPath } from 'url';
-import path, { dirname, join } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 8080;
 
-// Serve static files from the "public" directory
-app.use(express.static(join(__dirname, '..', 'public')));
+// Serve static files using express.static for the "public" directory
+app.use('/public', express.static(path.join(__dirname, 'Sudoku', 'public')));
 
-// Serve static files from the "dist/public" directory
-app.use(express.static(join(__dirname, 'public')));
 
-// Define a route for the root URL
-app.get('/', (req, res) => {
-  // Send the index.html file from the "public" directory
-  res.status(200).sendFile(join(__dirname, '..', 'public', 'index.html'));
+
+
+// Serve JS files from "Sudoku/dist/public"
+app.use('/Sudoku/dist/public/scripts.js', (req, res, next) => {
+  const filePath = path.join(__dirname, 'Sudoku', 'dist', 'public', 'scripts.js');
+  res.type('application/javascript');
+  res.sendFile(filePath);
 });
-app.get("/public/styles.css", (req, res) => {
-    res.type("text/css");
-    res.sendFile(path.join(__dirname, 'public', 'styles.css'));
-  });
 
-// Start the server
+// Serve CSS files from "Sudoku/public"
+app.use('/Sudoku/public/styles.css', (req, res, next) => {
+  const filePath = path.join(__dirname, 'Sudoku', 'public', 'styles.css');
+  res.type('text/css');
+  res.sendFile(filePath);
+});
+// Serve your HTML file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Sudoku', 'public', 'index.html'));
+});
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
